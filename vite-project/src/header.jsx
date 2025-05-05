749ac9f8b8a364743d46a8efc6df3dbe949657b2
Diego <iwujigoodluck5@gmail.com>
2025-05-05 06:55:18 -0700

    finished changing the images path into assets

--- vite-project/src/header.jsx
+++ vite-project/src/header.jsx
@@ -1,4 +1,12 @@
 import "./App.css";
+import hambuger from "./assets/topIcons/hamburger-menu.svg";
+import notification from "./assets/topIcons/notifications.svg";
+import search from "./assets/topIcons/search.svg";
+import upload from "./assets/topIcons/upload.svg";
+import voiceSearch from "./assets/topIcons/voice-search-icon.svg";
+import youtubeApps from "./assets/topIcons/youtube-apps.svg";
+import youtubeLogo from "./assets/topIcons/youtube-logo.svg";
+import myChannel from './assets/profilePics/my-channel.jpeg';
 
 function TopNav() {
   return (
@@ -7,12 +15,12 @@ function TopNav() {
         <div className=" flex w-[130px] justify-between items-center ">
           <div className=" size-6 cursor-pointer">
             <img
-              src="./public/top-icons/hamburger-menu.svg"
+              src={hambuger}
               alt="hamburger-menu"
             />
           </div>
           <div className="w-[90px] h-5 ">
-            <img src="./public/top-icons/youtube-logo.svg" alt="youtube-logo" />
+            <img src={youtubeLogo} alt="youtube-logo" />
           </div>
         </div>
 
@@ -25,7 +33,7 @@ function TopNav() {
             />
             <div className=" px-1 py-[15px] w-16 h-10 border-l border-[#cccccc] bg-[#f7f7f7] rounded-r-full flex justify-center items-center group ">
               <img
-                src="./public/top-icons/search.svg"
+                src={search}
                 alt="search icon"
                 className="size-8"
               />
@@ -36,7 +44,7 @@ function TopNav() {
           </div>
           <div className=" ml-2.5 size-10 p-2 rounded-full bg-[#f8f8f8] cursor-pointer flex justify-center group ">
             <img
-              src="./public/top-icons/voice-search-icon.svg"
+              src={voiceSearch}
               alt="voice-search-icon"
               className="size-6"
             />
@@ -48,20 +56,20 @@ function TopNav() {
 
         <div className=" flex w-[182px] justify-between items-center ">
           <div className=" relative size-6 cursor-pointer flex justify-center group ">
-            <img src="./public/top-icons/upload.svg" alt="video-upload" />
+            <img src={upload} alt="video-upload" />
             <div className=" -bottom-[50px] bg-[#606060e6] absolute text-white p-2 pointer-events-none rounded-[3px] text-[12px] transition-opacity ease-in-out opacity-0 group-hover:opacity-[1] whitespace-nowrap">
               Create
             </div>
           </div>
           <div className=" relative size-6 cursor-pointer flex justify-center group">
-            <img src="./public/top-icons/youtube-apps.svg" alt="youtube-apps" />
+            <img src={youtubeApps} alt="youtube-apps" />
             <div className=" -bottom-[50px] bg-[#606060e6] absolute text-white p-2 pointer-events-none rounded-[3px] text-[12px] transition-opacity ease-in-out opacity-0 group-hover:opacity-[1] whitespace-nowrap">
               YouTube Apps
             </div>
           </div>
           <div className=" relative size-6 cursor-pointer flex justify-center group">
             <img
-              src="./public/top-icons/notifications.svg"
+              src={notification}
               alt="notification icon"
             />
             <div className="red-notification absolute top-[-3px] right-[-4px] text-[10px] text-white rounded-full py-[1px] px-[5px] bg-[#b31313]  ">
@@ -73,7 +81,7 @@ function TopNav() {
           </div>
           <div className=" size-8">
             <img
-              src="./public/profilePics/my-channel.jpeg"
+              src={myChannel}
               alt="my channel profile"
             />
           </div>
