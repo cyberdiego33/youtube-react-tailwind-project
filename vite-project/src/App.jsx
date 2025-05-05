f21028117c1cf01566a838659184e7edb6b24f36
Diego <iwujigoodluck5@gmail.com>
2025-05-05 06:27:33 -0700

    i don see shege

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -13,11 +13,30 @@ import thumbnail9 from './assets/thumbnails/thumbnail-9.webp';
 import thumbnail10 from './assets/thumbnails/thumbnail-10.webp';
 import thumbnail11 from './assets/thumbnails/thumbnail-11.webp';
 import thumbnail12 from './assets/thumbnails/thumbnail-12.webp';
+import profile1 from './assets/profilePics/channel-1.jpeg';
+import profile2 from './assets/profilePics/channel-2.jpeg';
+import profile3 from './assets/profilePics/channel-3.jpeg';
+import profile4 from './assets/profilePics/channel-4.jpeg';
+import profile5 from './assets/profilePics/channel-5.jpeg';
+import profile6 from './assets/profilePics/channel-6.jpeg';
+import profile7 from './assets/profilePics/channel-7.jpeg';
+import profile8 from './assets/profilePics/channel-8.jpeg';
+import profile9 from './assets/profilePics/channel-9.jpeg';
+import profile10 from './assets/profilePics/channel-10.jpeg';
+import profile11 from './assets/profilePics/channel-11.jpeg';
+import profile12 from './assets/profilePics/channel-12.jpeg';
+import explore from './assets/sideIcons/explore.svg';
+import home from './assets/sideIcons/home.svg';
+import subscriptions from './assets/sideIcons/subscriptions.svg';
+import library from './assets/sideIcons/library.svg';
+import originals from './assets/sideIcons/originals.svg';
+import youtubeMusic from './assets/sideIcons/youtube-music.svg';
 
 const apiThumbnails = [
   {
     index: 1,
     thumbnail: thumbnail1,
+    profile: profile1,
     duration: "14:20",
     videoTitle: "Talking Tech and AI with Google CEO Sundar Pichai!",
     author: "Marques Brownlee",
@@ -27,6 +46,7 @@ const apiThumbnails = [
   {
     index: 2,
     thumbnail: thumbnail2,
+    profile: profile2,
     duration: "14:20",
     videoTitle: "Try Not To Laugh Challenge",
     author: "Marques Brownlee",
@@ -36,6 +56,7 @@ const apiThumbnails = [
   {
     index: 3,
     thumbnail: thumbnail3,
+    profile: profile3,
     duration: "8:22",
     videoTitle: "Crazy Tik Tok Taken Moments Before DISASTER",
     author: "SSSniperWolf",
@@ -45,6 +66,7 @@ const apiThumbnails = [
   {
     index: 4,
     thumbnail: thumbnail4,
+    profile: profile4,
     duration: "22:09",
     videoTitle: "Simple Mathematics That You Can't Solve",
     author: "Veritasium",
@@ -54,6 +76,7 @@ const apiThumbnails = [
   {
     index: 5,
     thumbnail: thumbnail5,
+    profile: profile5,
     duration: "8:22",
     videoTitle: "Kadane's Algorithm to Maximum Sum Subarray Problem",
     author: "CS Dojo",
@@ -63,6 +86,7 @@ const apiThumbnails = [
   {
     index: 6,
     thumbnail: thumbnail6,
+    profile: profile6,
     duration: "19:59",
     videoTitle: "Anything You Can Fit In The Circle I'll Pay For",
     author: "MrBeast",
@@ -72,6 +96,7 @@ const apiThumbnails = [
   {
     index: 7,
     thumbnail: thumbnail7,
+    profile: profile7,
     duration: "10:13",
     videoTitle: "Why Planes Don't Fly Over Tibet",
     author: "RealLofeLore",
@@ -81,6 +106,7 @@ const apiThumbnails = [
   {
     index: 8,
     thumbnail: thumbnail8,
+    profile: profile8,
     duration: "7:12",
     videoTitle: "Inside The World's Biggest Passenger Plane",
     author: "Tech Vision",
@@ -90,6 +116,7 @@ const apiThumbnails = [
   {
     index: 9,
     thumbnail: thumbnail9,
+    profile: profile9,
     duration: "13:17",
     videoTitle: "The Secret to Super Human STRENGHT",
     author: "ThenX",
@@ -99,6 +126,7 @@ const apiThumbnails = [
   {
     index: 10,
     thumbnail: thumbnail10,
+    profile: profile10,
     duration: "7:53",
     videoTitle:
       "How The World's Largest Cruise Ship Makes 30,000 Meals Every Day",
@@ -109,6 +137,7 @@ const apiThumbnails = [
   {
     index: 11,
     thumbnail: thumbnail11,
+    profile: profile11,
     duration: "4:10",
     videoTitle:
       "Dubai's Crazy Underwater Train and Other Things #Only in Dubia",
@@ -119,6 +148,7 @@ const apiThumbnails = [
   {
     index: 12,
     thumbnail: thumbnail12,
+    profile: profile12,
     duration: "4:51",
     videoTitle: "What would happen if you didn't drink water? Mia Nacamulli",
     author: "TED-ED",
@@ -129,27 +159,27 @@ const apiThumbnails = [
 
 const apiSideIcons = [
   {
-    icon: "home",
+    icon: home,
     label: "Home",
   },
   {
-    icon: "explore",
+    icon: explore,
     label: "Explore",
   },
   {
-    icon: "subscriptions",
+    icon: subscriptions,
     label: "Subscriptions",
   },
   {
-    icon: "originals",
+    icon: originals,
     label: "Originals",
   },
   {
-    icon: "youtube-music",
+    icon: youtubeMusic,
     label: "Youtube Music",
   },
   {
-    icon: "library",
+    icon: library,
     label: "Library",
   },
 ];
@@ -200,7 +230,7 @@ function GridVidArea() {
             thumbimage={item.thumbnail}
             index={item.index}
             duration={item.duration}
-            profilePic={`./public/profilePics/channel-${item.index}.jpeg`}
+            profilePic={item.profile}
             vidTitle={item.videoTitle}
             youtuber={item.author}
             views={item.view}
@@ -216,7 +246,7 @@ function SideIcons(props) {
   return (
     <div class="sideicons-div h-[75px] flex flex-col items-center justify-center cursor-pointer hover:bg-[#ebebeb]">
       <img
-        src={`./public/side-icons/${props.icon}.svg`}
+        src={props.icon}
         alt="home"
         className="size-6 mb-1"
       />
